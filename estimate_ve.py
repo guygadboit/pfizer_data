a41b008bf09d9a2b46843b74076f171d981a8b73
Guy Gadboit <guy@invalid.org>
2022-05-26 09:12:45 +0100

    Check for arm switchers
    But there aren't any.

--- estimate_ve.py
+++ estimate_ve.py
@@ -3,9 +3,10 @@ import csv
 import gzip
 from collections import namedtuple
 from math import *
+from datetime import *
 from pdb import set_trace as brk
 
-Datum = namedtuple("Datum", "row_num subj_id arm when what result phase")
+Datum = namedtuple("Datum", "row_num subj_id arm when what result phase date")
 
 def convert_col(col):
 	"convert a column index like AM back to decimal"
@@ -20,26 +21,39 @@ def convert_col(col):
 
 	return ret
 
+def convert_date(s):
+	try:
+		return datetime.strptime(s, "%Y-%m-%d")
+	except ValueError:
+		return datetime(2020, 1, 1)
+
 def parse_row(row, num):
 	cols = (
-		('D', 'subj_id'),
-		('F', 'arm'),
-		('O', 'when'),
-		('S', 'what'),
-		('W', 'result'),
-		('DE', 'phase'),
+		('D', 'subj_id', None),
+		('H', 'arm', None),
+		('O', 'when', None),
+		('S', 'what', None),
+		('W', 'result', None),
+		('DE', 'phase', None),
+		('J', 'date', convert_date),
 		)
 	fields = {"row_num": num}
-	for index, field in cols:
-		fields[field] = row[convert_col(index)]
+	for index, field, convert in cols:
+		d = row[convert_col(index)]
+		if convert: d = convert(d)
+		fields[field] = d
 	return Datum(**fields)
 
-def load_data(filters):
-	"Generate datums from the rows that match all the filters"
+def load_data(filters, date=None):
+	"""Generate datums from the rows that match all the filters where date is <
+	date"""
 	with gzip.open("adva.csv.gz", "rt") as fp:
 		r = csv.reader(fp)
 		for i, row in enumerate(r):
 			datum = parse_row(row, i+1)
+
+			if date and datum.date >= date:
+				continue
 
 			for attr, val in filters.items():
 				if getattr(datum, attr) != val:
@@ -65,7 +79,7 @@ def ci(a, b, c, d):
 def to_ve(rr):
 	return (1-rr) * 100
 
-def calc_ve():
+def calc_ve(date=None):
 	filters = {
 			"what": "N-binding antibody - N-binding Antibody Assay",
 			"when": "V1_DAY1_VAX1_L",
@@ -75,31 +89,38 @@ def calc_ve():
 
 	# Find the people who were negative to start with, and the totals in each
 	# arm
-	group = set()
-	placebo_total, vax_total = 0, 0
+	placebo_arm, vax_arm = set(), set()
 
-	for datum in load_data(filters):
-		if datum.subj_id in group: continue
-		group.add(datum.subj_id)
+	for datum in load_data(filters, date):
 		if datum.arm == "Placebo":
-			placebo_total += 1
+			placebo_arm.add(datum.subj_id)
 		else:
-			vax_total += 1
-
-	print("Found {} interesting people".format(len(group)))
+			vax_arm.add(datum.subj_id)
 
 	# Now see how many in each arm became positive afterwards
 	placebo, vax = 0, 0
 	filters.update({"when": "V3_MONTH1_POSTVAX2_L", "result": "POS"})
-	for datum in load_data(filters):
+	for datum in load_data(filters, date):
 
-		if datum.subj_id not in group:
-			continue
+		if datum.subj_id in placebo_arm:
+			if datum.arm != "Placebo":
+				print("{} switched out of Placebo arm".format(datum.subj_id))
+				placebo_arm.remove(datum.subj_id)
+			else:
+				placebo += 1
 
-		if datum.arm == "Placebo":
-			placebo += 1
-		else:
-			vax += 1
+		elif datum.subj_id in vax_arm:
+			if datum.arm == "Placebo":
+				print("{} switched INTO Placebo arm!".format(datum.subj_id))
+				vax_arm.remove(datum.subj_id)
+			else:
+				vax += 1
+
+	if date:
+		print("Considering only rows before {}".format(date))
+
+	vax_total = len(vax_arm)
+	placebo_total = len(placebo_arm)
 
 	print("{} / {} in vax arm went from NEG to POS".format(vax, vax_total))
 	print("{} / {} in placebo arm went from NEG to POS".format(placebo,
@@ -112,7 +133,8 @@ def calc_ve():
 	print([to_ve(r) for r in results])
 
 def main():
-	calc_ve()
+# 	calc_ve()
+	calc_ve(datetime(2020, 11, 15))
 
 if __name__ == "__main__":
 	main()
